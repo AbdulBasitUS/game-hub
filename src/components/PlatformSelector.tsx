45b3c7e88ea005ba7d209aba6fe9da6577a01586
Abdul Basit <basit258906@gmail.com>
2023-08-29 23:19:52 +0530

    Filter by platform and Refactor: Extract a query object

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,16 +1,24 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
 import { BsChevronDown } from 'react-icons/bs'
 import usePlatforms from '../hooks/usePlatforms'
+import { Platform } from './../hooks/useGames';
 
-const PlatformSelector = () => {
+interface Props {
+    onSelectPlatform: (platform: Platform) => void;
+    selectedPlatform: Platform | null;
+}
+
+const PlatformSelector = ({ selectedPlatform, onSelectPlatform }: Props) => {
     const { data: platforms, error } = usePlatforms()
 
     if (error) return null
   return (
     <Menu>
-        <MenuButton as={Button} rightIcon={<BsChevronDown />}>Platform</MenuButton>
+        <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+            {selectedPlatform?.name || 'Platform'}
+        </MenuButton>
         <MenuList>
-            {platforms.map(platform => <MenuItem key={platform.id}>{platform.name}</MenuItem>)}
+            {platforms.map(platform => <MenuItem key={platform.id} onClick={() => onSelectPlatform(platform)} >{platform.name}</MenuItem>)}
         </MenuList>
     </Menu>
   )
